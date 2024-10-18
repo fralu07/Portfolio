ea7c7f827a05f4e9082dba1f7d0e560b8df02fcf
Francesco Lucia <francesco.lucia@students.fhnw.ch>
2024-10-18 19:32:33 +0200

    fix: Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customGray: '#b0a8a2',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
+
