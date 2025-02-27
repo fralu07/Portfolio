760b2cb0c51113de1f47ecd8b99ecd82a2b3cc18
Francesco Lucia <francesco.lucia@students.fhnw.ch>
2025-02-27 11:35:29 +0100

    change title application

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
                         >
                             <span className="text-sm md:text-xl">Bewerbung als </span>
                             <br/>
-                            Fullstack Engineer
+                            Leiter ICT und Digitalisierung
                         </motion.h2>
                         <div className="flex flex-col gap-10 items-center">
                             <motion.div className="flex flex-row gap-5 pt-8" variants={textVariants}>
