aec2d93c7b49acb96f6a16e799fa104a3590c9c5
Francesco Lucia <francesco.lucia@students.fhnw.ch>
2025-03-13 15:45:54 +0100

    aveniq app

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -46,7 +46,8 @@ const Hero = () => {
                         >
                             <span className="text-sm md:text-xl">Bewerbung als </span>
                             <br/>
-                            Team Leader Project & Program Mgmt
+                            Team Leader Project & <br/>
+                            Program Management
                         </motion.h2>
                         <div className="flex flex-col gap-10 items-center">
                             <motion.div className="flex flex-row gap-5 pt-8" variants={textVariants}>
